464ff514878cf51e9028d2b36c22b750ef9eb1f3
Gareth Brickman <brickstick@gmail.com>
2020-07-22 08:34:17 -0400

    Complete 100.

--- 0x00-python_variable_annotations/100-safe_first_element.py
+++ 0x00-python_variable_annotations/100-safe_first_element.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
 """ Annotates function params and return values """
 
-from typing import List, Tuple
+from typing import Sequence, Union, Any
 
 
-def safe_first_element(lst):
+def safe_first_element(lst: Sequence[Any]) -> Union[Any, None]:
     """ Duck-typing """
     if lst:
         return lst[0]
