5c1a5f98f67a5f762876efdbd987978cef72f01b
Gareth Brickman <brickstick@gmail.com>
2020-07-26 16:39:42 -0400

    Init commit

---
+++ 0x03-caching/0-basic_cache.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+""" Python caching systems """
+
+from base_caching import BaseCaching
+
+
+class BasicCache(BaseCaching):
+    """ Dict caching system """
+
+    def put(self, key, item):
+        """ puts """
+        pass
+
+    def get(self, key):
+        """ gets """
+        pass
