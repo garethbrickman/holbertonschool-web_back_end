6fdf8631b16bb80d34b366744914aed738299029
Gareth Brickman <brickstick@gmail.com>
2020-07-26 15:47:38 -0400

    Complete code

--- 0x01-python_async_function/3-tasks.py
+++ 0x01-python_async_function/3-tasks.py
@@ -1,13 +1,11 @@
 #!/usr/bin/env python3
-""" Takes int arg, waits for random delay """
+""" Takes int arg, returns random delay """
 
 import asyncio
 import random
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-async def wait_random(max_delay: int = 10) -> float:
-    """ Waits for random delay between 0 and max_delay, returns that """
-    actual_delay: float = random.uniform(0, max_delay)
-    await asyncio.sleep(actual_delay)
-    return actual_delay
+def task_wait_random(max_delay: int = 10) -> asyncio.Task:
+    """ Waits for random delay then returns asyncio.Task object """
+    return asyncio.create_task(wait_random(max_delay))
