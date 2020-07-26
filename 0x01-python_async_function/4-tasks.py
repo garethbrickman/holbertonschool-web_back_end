46778ef9a7614e432029446e1f3098dbd604c846
Gareth Brickman <brickstick@gmail.com>
2020-07-26 15:52:09 -0400

    Complete code

--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -1,13 +1,22 @@
 #!/usr/bin/env python3
 """ Takes int arg, waits for random delay """
 
+from typing import List
 import asyncio
 import random
-wait_random = __import__('0-basic_async_syntax').wait_random
+task_wait_random = __import__('3-tasks').task_wait_random
 
 
-async def wait_random(max_delay: int = 10) -> float:
-    """ Waits for random delay between 0 and max_delay, returns that """
-    actual_delay: float = random.uniform(0, max_delay)
-    await asyncio.sleep(actual_delay)
-    return actual_delay
+async def task_wait_n(n: int, max_delay: int = 10) -> List[float]:
+    """ Waits for ran delay until max_delay, returns list of actual delays """
+    spawn_list = []
+    delay_list = []
+    for i in range(n):
+        delayed_task = task_wait_random(max_delay)
+        delayed_task.add_done_callback(lambda x: delay_list.append(x.result()))
+        spawn_list.append(delayed_task)
+
+    for spawn in spawn_list:
+        await spawn
+
+    return delay_list
