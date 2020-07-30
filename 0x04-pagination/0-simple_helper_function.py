8be0d881aa26baeb34bbaf5c9017893ae37bafe8
Gareth Brickman <brickstick@gmail.com>
2020-07-29 20:39:46 -0400

    Test typing

--- 0x04-pagination/0-simple_helper_function.py
+++ 0x04-pagination/0-simple_helper_function.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python3
 """ Pagination """
 
-def index_range(page, page_size):
+from typing import Tuple
+
+
+def index_range(page: int, page_size: int) -> Tuple[int, int]:
     """ returns tuple of size two containing a start index and an end index """
     pass
