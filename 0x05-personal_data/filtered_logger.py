34be97a3d8458ef8e755cf104df8b530bbd3b6dd
Gareth Brickman <brickstick@gmail.com>
2020-08-03 22:49:57 -0400

    Init commit

---
+++ 0x05-personal_data/filtered_logger.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+""" Protecting PII """
+
+from typing import List
+
+
+def filter_datum(fields: List[str], redaction: str,
+                 message: str, separator: str) -> str
+    """ returns the log message obfuscated """
+    pass
