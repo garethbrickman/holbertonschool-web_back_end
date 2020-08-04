41b8c66fc7a536fbb4fb924aad0bd981d4793ec9
Gareth Brickman <brickstick@gmail.com>
2020-08-03 23:18:40 -0400

    Incl all doc strings:

--- 0x05-personal_data/filtered_logger.py
+++ 0x05-personal_data/filtered_logger.py
@@ -22,10 +22,12 @@ class RedactingFormatter(logging.Formatter):
     SEPARATOR = ";"
 
     def __init__(self):
+        """ init """
         super(RedactingFormatter, self).__init__(self.FORMAT)
         self.fields = fields
 
     def format(self, record: logging.LogRecord) -> str:
+        """ filters values in incoming log records """
         return filter_datum(
             self.fields, self.REDACTION, super(
                 RedactingFormatter, self).format(record),
