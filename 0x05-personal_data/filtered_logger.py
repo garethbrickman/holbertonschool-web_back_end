9b774ce5812c540ab5f48d1dcd50d04a8f72e3c0
Gareth Brickman <brickstick@gmail.com>
2020-08-03 23:04:46 -0400

    Add 0.

--- 0x05-personal_data/filtered_logger.py
+++ 0x05-personal_data/filtered_logger.py
@@ -2,9 +2,12 @@
 """ Protecting PII """
 
 from typing import List
+import re
 
 
 def filter_datum(fields: List[str], redaction: str,
-                 message: str, separator: str) -> str
+                 message: str, separator: str) -> str:
     """ returns the log message obfuscated """
-    pass
+    for field in fields:
+        return re.sub(field + "=.*?" + separator,
+                      field + "=" + redaction + separator, message)
