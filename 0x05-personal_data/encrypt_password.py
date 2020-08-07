fa9572cc49bc5d968de14ceb0c1f03ffa4cb9520
Gareth Brickman <brickstick@gmail.com>
2020-08-07 15:46:04 -0400

    Add - to utf8

--- 0x05-personal_data/encrypt_password.py
+++ 0x05-personal_data/encrypt_password.py
@@ -8,14 +8,14 @@ def hash_password(password: str) -> bytes:
     """ Takes in string arg, converts to unicode
     Returns salted, hashed pswd as bytestring
     """
-    return bcrypt.hashpw(password.encode('utf8'), bcrypt.gensalt())
+    return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
 
 
 def is_valid(hash_password: bytes, password: str) -> bool:
     """ Checks if hashed and unhashed pswds are same
     Returns bool
     """
-    if bcrypt.checkpw(password.encode('utf8'), hash_password):
+    if bcrypt.checkpw(password.encode('utf-8'), hash_password):
         return True
     else:
         return False
