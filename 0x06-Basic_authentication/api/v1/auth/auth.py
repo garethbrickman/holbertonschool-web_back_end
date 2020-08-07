7c28bb28f0a303093baebb874ad134235226db93
Gareth Brickman <brickstick@gmail.com>
2020-08-07 17:20:06 -0400

    Add 4.

--- 0x06-Basic_authentication/api/v1/auth/auth.py
+++ 0x06-Basic_authentication/api/v1/auth/auth.py
@@ -9,9 +9,17 @@ from typing import List, TypeVar
 
 class Auth:
     """ Authentication """
+
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
-        """ placeholder """
-        return False
+        """ Checks if API routes require authentication """
+        if path is None or excluded_paths is None or len(excluded_paths) == 0:
+            return True
+        if path[-1] != '/':
+            path += '/'
+        if path in excluded_paths:
+            return False
+        else:
+            return True
 
     def authorization_header(self, request=None) -> str:
         """ placeholder """
