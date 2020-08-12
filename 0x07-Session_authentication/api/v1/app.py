ad40cb60a89ed0f73d3f88a6b54e7ad7d1ef04ff
Gareth Brickman <brickstick@gmail.com>
2020-08-11 21:24:29 -0400

    Add 0.

--- 0x07-Session_authentication/api/v1/app.py
+++ 0x07-Session_authentication/api/v1/app.py
@@ -36,6 +36,7 @@ def request_filter() -> None:
         if auth.require_auth(request.path, excluded_paths):
             if auth.authorization_header(request) is None:
                 abort(401)
+            request.current_user = auth.current_user(request) 
             if auth.current_user(request) is None:
                 abort(403)
 
