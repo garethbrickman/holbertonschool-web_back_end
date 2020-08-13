b9abbda2b6b04b11de7bdd83c614c64b1e381812
Gareth Brickman <brickstick@gmail.com>
2020-08-12 20:06:09 -0400

    Fix pep8

--- 0x07-Session_authentication/api/v1/app.py
+++ 0x07-Session_authentication/api/v1/app.py
@@ -39,11 +39,12 @@ def request_filter() -> None:
     if auth:
         if auth.require_auth(request.path, excluded_paths):
             if auth.authorization_header(request) is None and\
-                auth.session_cookie(request) is None:
+                    auth.session_cookie(request) is None:
                 abort(401)
-            request.current_user = auth.current_user(request) 
+            request.current_user = auth.current_user(request)
             if auth.current_user(request) is None:
                 abort(403)
+
 
 @app.errorhandler(404)
 def not_found(error) -> str:
--- 0x07-Session_authentication/api/v1/auth/session_auth.py
+++ 0x07-Session_authentication/api/v1/auth/session_auth.py
@@ -12,18 +12,18 @@ class SessionAuth(Auth):
     """ Session Authentication """
 
     user_id_by_session_id = {}
-    
+
     def create_session(self, user_id: str = None) -> str:
         """ Creates a Session ID for user_id """
 
         if user_id is None or isinstance(user_id, str) is False:
             return None
-            
+
         session_id = str(uuid.uuid4())
         self.user_id_by_session_id[session_id] = user_id
 
         return session_id
-    
+
     def user_id_for_session_id(self, session_id: str = None) -> str:
         """ Returns User ID based on Session ID """
 
@@ -34,7 +34,7 @@ class SessionAuth(Auth):
 
     def current_user(self, request=None):
         """ Returns a User instance based on cookie value """
-        
+
         session_id = self.session_cookie(request)
 
         return User.get(self.user_id_for_session_id(session_id))
