c0d33920296845cda4631bbad937728523ccd4d2
Gareth Brickman <brickstick@gmail.com>
2020-08-18 22:23:29 -0400

    Move checkpw back to where it was before

--- 0x08-user_authentication_service/auth.py
+++ 0x08-user_authentication_service/auth.py
@@ -35,6 +35,7 @@ class Auth:
         """ Checks if user pswd is valid, locating by email"""
         try:
             found_user = self._db.find_user_by(email=email)
+            return checkpw(password.encode('utf-8'), found_user.hashed_password)
         except NoResultFound:
             return False
-        return checkpw(password.encode('utf-8'), found_user.hashed_password)
+
