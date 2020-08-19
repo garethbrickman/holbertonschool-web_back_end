336e8600656b19b3eed5b8a9f66911c5aadc9aff
Gareth Brickman <brickstick@gmail.com>
2020-08-19 13:39:57 -0400

    Add 11.

--- 0x08-user_authentication_service/app.py
+++ 0x08-user_authentication_service/app.py
@@ -21,7 +21,7 @@ def status() -> str:
 @app.route('/users', methods=['POST'], strict_slashes=False)
 def users() -> str:
     """ POST /users
-    Registers new user with email and pswd in request form-data,
+    Registers new user with email and pswd in x-www-form-urlencoded request,
     or finds if user already registered based on email
     Return:
       - JSON payload
@@ -47,6 +47,38 @@ def users() -> str:
             })
         except ValueError:
             return jsonify({"message": "email already registered"}), 400
+
+
+@app.route('/sessions', methods=['POST'], strict_slashes=False)
+def login() -> str:
+    """ POST /sessions
+    Creates new session for user, stores as cookie
+    Email and pswd fields in x-www-form-urlencoded request
+    Return:
+      - JSON payload
+    """
+    form_data = request.form
+
+    if "email" not in form_data:
+        return jsonify({"message": "email required"}), 400
+    elif "password" not in form_data:
+        return jsonify({"message": "password required"}), 400
+    else:
+
+        email = request.form.get("email")
+        pswd = request.form.get("password")
+
+        if AUTH.valid_login(email, pswd) is False:
+            abort(401)
+        else:
+            session_id = AUTH.create_session(email)
+            response = jsonify({
+                "email": email,
+                "message": "logged in"
+                })
+            response.set_cookie('session_id', session_id)
+
+            return response
 
 
 if __name__ == "__main__":
