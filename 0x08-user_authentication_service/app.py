d629ac950c24e8e6c1ed4c34b9ad6ca655051b3a
Gareth Brickman <brickstick@gmail.com>
2020-08-19 18:24:59 -0400

    Add 17.

--- 0x08-user_authentication_service/app.py
+++ 0x08-user_authentication_service/app.py
@@ -120,5 +120,31 @@ def profile() -> str:
         abort(403)
 
 
+@app.route('/reset_password', methods=['POST'], strict_slashes=False)
+def get_reset_password_token() -> str:
+    """ POST /reset_password
+    Generates pswd reset token
+    Email field in x-www-form-urlencoded request
+    Return:
+      - JSON payload
+    """
+    form_data = request.form
+
+    if "email" not in form_data:
+        return jsonify({"message": "email required"}), 400
+    else:
+
+        email = request.form.get("email")
+
+        try:
+            reset_token = AUTH.get_reset_password_token(email)
+            return jsonify({
+                "email": email,
+                "reset_token": reset_token
+            }), 200
+        except ValueError:
+            abort(403)
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
