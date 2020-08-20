51d8915f4da2328d9783b4619bfec9dc6aad0fd7
Gareth Brickman <brickstick@gmail.com>
2020-08-19 20:03:39 -0400

    Change order of try/except operations

--- 0x08-user_authentication_service/auth.py
+++ 0x08-user_authentication_service/auth.py
@@ -96,12 +96,9 @@ class Auth:
     def update_password(self, reset_token: str, password: str) -> None:
         """ Finds user by reset_token, updates user's pswd """
         try:
-            found_user = self._db.find_user_by(reset_token=reset_token)
+            user = self._db.find_user_by(reset_token=reset_token)
+            hp = _hash_password(password)
+            self._db.update_user(user.id, reset_token=None, hashed_password=hp)
+            return None
         except NoResultFound:
             raise ValueError
-        new_pswd = _hash_password(password)
-        self._db.update_user(
-            found_user.id,
-            hashed_password=new_pswd,
-            reset_token=None)
-        return None
