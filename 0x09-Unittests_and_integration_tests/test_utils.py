6f453af39519d6c3c8397b74946bfcf1d8a3be30
Gareth Brickman <brickstick@gmail.com>
2020-08-22 18:30:20 -0400

    Add 1.

--- 0x09-Unittests_and_integration_tests/test_utils.py
+++ 0x09-Unittests_and_integration_tests/test_utils.py
@@ -9,12 +9,23 @@ from utils import access_nested_map
 
 class TestAccessNestedMap(unittest.TestCase):
     """ Class for testing Nested Map function """
+    # order of args: map, path, expected_output
     @parameterized.expand([
         ({"a": 1}, ("a",), 1),
         ({"a": {"b": 2}}, ("a",), {'b': 2}),
         ({"a": {"b": 2}}, ("a", "b"), 2)
     ])
     def test_access_nested_map(self, map, path, expected_output):
-        """ Test method return output """
+        """ Test method returns correct output """
         real_output = access_nested_map(map, path)
         self.assertEqual(real_output, expected_output)
+
+    @parameterized.expand([
+        ({}, ("a",), 'a'),
+        ({"a": 1}, ("a", "b"), 'b')
+    ])
+    def test_access_nested_map_exception(self, map, path, wrong_output):
+        """ Test method raises correct exception """
+        with self.assertRaises(KeyError) as e:
+            access_nested_map(map, path)
+            self.assertEqual(wrong_output, e.exception)
