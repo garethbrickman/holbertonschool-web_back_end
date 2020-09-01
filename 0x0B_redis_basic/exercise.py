9529868a81e76abd3aed5a2b36592cb373278b11
Gareth Brickman <brickstick@gmail.com>
2020-09-01 10:59:50 -0400

    Add 0.

--- 0x0B_redis_basic/exercise.py
+++ 0x0B_redis_basic/exercise.py
@@ -1,20 +1,22 @@
 #!/usr/bin/env python3
 """ Module for Redis db """
+import redis
 from uuid import uuid4
-
 from typing import Union
 
 
 class Cache:
-    """ Placeholder text """
+    """ Class for methods that operate a caching system """
 
     def __init__(self):
-        """ Placeholder text """
-        #_redis = instanceOfRedisClient
-        # #flushdb()
+        """ Instance of Redis db """
+        self._redis = redis.Redis()
+        self._redis.flushdb()
 
     def store(self, data: Union[str, bytes, int, float]) -> str:
-        """ Placeholder text """
-        key = uuid4()
-        #store input data in Redis using key
+        """ Creates key and stores it with data """
+        # uuid must be type cast to str for Redis to be able to accept it
+        key = str(uuid4())
+        # use pipelining for multi sets, mset() is not approrpiate for a cache
+        self._redis.set(key, data)
         return key
