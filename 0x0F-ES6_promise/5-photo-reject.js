73ceffb22aa5b7b1f43c5259bdbbc4ebcacdcfe6
Gareth Brickman <brickstick@gmail.com>
2020-09-16 22:49:44 -0400

    Fix with .reject syntax

--- 0x0F-ES6_promise/5-photo-reject.js
+++ 0x0F-ES6_promise/5-photo-reject.js
@@ -1,6 +1,3 @@
 export default function uploadPhoto(filename) {
-  const myPromise = new Promise((reject) => {
-    reject(new Error(`${filename} cannot be processed`));
-  });
-  return myPromise;
+  return Promise.reject(new Error(`${filename} cannot be processed`));
 }
