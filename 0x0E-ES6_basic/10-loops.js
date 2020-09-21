57852b77ebf2fdbd9fe39726893b882a8dd7026a
Gareth Brickman <brickstick@gmail.com>
2020-09-21 09:18:18 -0400

    Fix 10.

--- 0x0E-ES6_basic/10-loops.js
+++ 0x0E-ES6_basic/10-loops.js
@@ -1,8 +1,7 @@
 export default function appendToEachArrayValue(array, appendString) {
   const array2 = [];
-  for (const [idx, item] of array.entries()) {
-    array2[idx] = appendString + item;
+  for (const value of array) {
+    array2.push(appendString + value);
   }
-
   return array2;
 }
