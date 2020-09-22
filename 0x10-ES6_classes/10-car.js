55c8747507a74b1c6e8b001d4dc94f92cd314147
Gareth Brickman <brickstick@gmail.com>
2020-09-22 18:46:42 -0400

    Fix eslint

--- 0x10-ES6_classes/10-car.js
+++ 0x10-ES6_classes/10-car.js
@@ -9,10 +9,18 @@ export default class Car {
   // Methods
 
   cloneCar() {
-    return this.constructor[Symbol.species];
+    const NewObj = this.constructor[Symbol.species] || this.constructor;
+    const clone = new NewObj();
+    return clone;
   }
 
   // Setters
 
   // Getters
 }
+/* class TestCar extends Car {};
+const tc1 = new TestCar("Nissan", "Turbo", "Pink");
+const tc2 = tc1.cloneCar();
+console.log(tc1 instanceof TestCar);
+console.log(tc2 instanceof TestCar);
+console.log(tc1 == tc2); */
