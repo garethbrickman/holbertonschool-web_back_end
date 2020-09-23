06375fe6ab6f6b87106684df4d60d0c8b91dab89
Gareth Brickman <brickstick@gmail.com>
2020-09-23 10:52:52 -0400

    Fix eslint

--- 0x11-ES6_data_manipulation/1-get_list_student_ids.js
+++ 0x11-ES6_data_manipulation/1-get_list_student_ids.js
@@ -2,5 +2,6 @@ export default function getListStudentIds(students) {
   // check arg is an array before using map
   if (Object.getPrototypeOf(students) === Array.prototype) {
     return students.map((items) => items.id);
-  } return [];
+  }
+  return [];
 }
