da21778493550427eee938d64d91e973b7f7979f
Gareth Brickman <brickstick@gmail.com>
2020-09-24 09:09:42 -0400

    Fix array check

--- 0x11-ES6_data_manipulation/1-get_list_student_ids.js
+++ 0x11-ES6_data_manipulation/1-get_list_student_ids.js
@@ -1,6 +1,6 @@
 export default function getListStudentIds(students) {
   // check arg is an array before using map
-  if (Object.getPrototypeOf(students) === Array.prototype) {
+  if (Array.isArray(students)) {
     return students.map((items) => items.id);
   }
   return [];
