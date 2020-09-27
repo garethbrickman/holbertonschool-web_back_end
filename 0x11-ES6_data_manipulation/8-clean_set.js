20254a94fcfabedca41a7892eea00d3798819fe7
Gareth Brickman <brickstick@gmail.com>
2020-09-27 16:49:15 -0400

    Slice substring by length of setString

--- 0x11-ES6_data_manipulation/8-clean_set.js
+++ 0x11-ES6_data_manipulation/8-clean_set.js
@@ -1,12 +1,16 @@
-export default function cleanSet(set, startString) {
+/* export default function cleanSet(set, startString) { */
+function cleanSet() {
+  let set = new Set(['bonjovi', 'bonaparte', 'bonappetit', 'banana']);
+  let startString = 'bon';
   if (startString && typeof startString === 'string') {
     const strings = [];
     for (const item of set) {
       if (item.startsWith(startString)) {
-        strings.push(item);
+        strings.push(item.slice(startString.length));
       }
     }
     return strings.join('-');
   }
   return '';
 }
+console.log(cleanSet());
