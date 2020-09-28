78d570ca80dc2276ffbd429d7e1c163dd4b5208d
Gareth Brickman <brickstick@gmail.com>
2020-09-28 17:50:54 -0400

    Remove repetition

--- 0x12-Node_JS_basic/1-stdin.js
+++ 0x12-Node_JS_basic/1-stdin.js
@@ -1,15 +1,14 @@
 console.log('Welcome to Holberton School, what is your name?');
+// await input
 process.stdin.resume();
+// once input is entered
 process.stdin.on('readable', () => {
+  const name = process.stdin.read();
+  process.stdout.write(`Your name is: ${name}`);
   // check if input is coming from the terminal
   if (process.stdin.isTTY) {
-    const name = process.stdin.read();
-    process.stdout.write(`Your name is: ${name}`);
     process.exit();
-  // not coming from the terminal
   } else {
-    const name = process.stdin.read();
-    process.stdout.write(`Your name is: ${name}`);
     process.stdout.write('This important software is now closing\n');
     process.exit();
   }
