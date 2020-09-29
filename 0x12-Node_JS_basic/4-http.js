4b49bf45db052968a342efa68d76a74b7dbfef1c
Gareth Brickman <brickstick@gmail.com>
2020-09-28 21:13:05 -0400

    Fix so that response displays text, not console

--- 0x12-Node_JS_basic/4-http.js
+++ 0x12-Node_JS_basic/4-http.js
@@ -6,11 +6,7 @@ const port = 1245;
 const app = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World');
+  res.end('Hello Holberton School!');
 });
 
-app.listen(port, hostname, () => {
-  console.log('Hello Holberton School!');
-});
-
-module.exports = app;
+app.listen(port, hostname);
