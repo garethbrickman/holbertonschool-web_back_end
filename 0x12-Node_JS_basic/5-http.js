f1aedc8579614a0a4800715398054eee8820d6ef
Gareth Brickman <brickstick@gmail.com>
2020-09-29 18:25:16 -0400

    Add back catch error in async func

--- 0x12-Node_JS_basic/5-http.js
+++ 0x12-Node_JS_basic/5-http.js
@@ -2,49 +2,53 @@ const http = require('http');
 const fs = require('fs');
 
 async function countStudents(filepath) {
-  const csv = await fs.promises.readFile(filepath, { encoding: 'utf8' });
-  const headerArray = csv.split(/\r?\n|\n/);
-  const headers = headerArray[0].split(',');
+  try {
+    const csv = await fs.promises.readFile(filepath, { encoding: 'utf8' });
+    const headerArray = csv.split(/\r?\n|\n/);
+    const headers = headerArray[0].split(',');
 
-  // strip headers and convert to list of dicts
-  const dictList = [];
-  const noHeaderArray = headerArray.slice(1);
-  for (let i = 0; i < noHeaderArray.length; i += 1) {
-    const data = noHeaderArray[i].split(',');
-    if (data.length === headers.length) {
-      const row = {};
-      for (let j = 0; j < headers.length; j += 1) {
-        row[headers[j].trim()] = data[j].trim();
+    // strip headers and convert to list of dicts
+    const dictList = [];
+    const noHeaderArray = headerArray.slice(1);
+    for (let i = 0; i < noHeaderArray.length; i += 1) {
+      const data = noHeaderArray[i].split(',');
+      if (data.length === headers.length) {
+        const row = {};
+        for (let j = 0; j < headers.length; j += 1) {
+          row[headers[j].trim()] = data[j].trim();
+        }
+        dictList.push(row);
       }
-      dictList.push(row);
     }
+
+    // count and collect first names of students per field
+    let countCS = 0;
+    let countSWE = 0;
+    const studentsCS = [];
+    const studentsSWE = [];
+
+    dictList.forEach((element) => {
+      if (element.field === 'CS') {
+        countCS += 1;
+        studentsCS.push(element.firstname);
+      } else if (element.field === 'SWE') {
+        countSWE += 1;
+        studentsSWE.push(element.firstname);
+      }
+    });
+
+    const countStudents = countCS + countSWE;
+
+    return ({
+      countStudents,
+      countCS,
+      countSWE,
+      studentsCS,
+      studentsSWE,
+    });
+  } catch (err) {
+    throw new Error('Cannot load the database');
   }
-
-  // count and collect first names of students per field
-  let countCS = 0;
-  let countSWE = 0;
-  const studentsCS = [];
-  const studentsSWE = [];
-
-  dictList.forEach((element) => {
-    if (element.field === 'CS') {
-      countCS += 1;
-      studentsCS.push(element.firstname);
-    } else if (element.field === 'SWE') {
-      countSWE += 1;
-      studentsSWE.push(element.firstname);
-    }
-  });
-
-  const countStudents = countCS + countSWE;
-
-  return ({
-    countStudents,
-    countCS,
-    countSWE,
-    studentsCS,
-    studentsSWE,
-  });
 }
 
 const pathToDB = process.argv[2];
