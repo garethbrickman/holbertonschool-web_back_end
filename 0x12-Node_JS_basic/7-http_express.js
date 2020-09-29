9a6b19dab0add0505af6f9b93ac24ffe169f9c7f
Gareth Brickman <brickstick@gmail.com>
2020-09-28 22:33:02 -0400

    Add 7.

---
+++ 0x12-Node_JS_basic/7-http_express.js
@@ -0,0 +1,16 @@
+const express = require('express');
+
+const app = express();
+const port = 1245;
+
+app.get('/', (req, res) => {
+  res.send('Hello Holberton School!');
+});
+
+app.get('/students', (req, res) => {
+  res.end('This is the list of our students');
+});
+
+app.listen(port);
+
+module.exports = app;
