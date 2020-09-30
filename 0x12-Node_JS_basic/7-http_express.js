4ad7709da06fa96c03b142e61ecc973caa76e7d2
Gareth Brickman <brickstick@gmail.com>
2020-09-29 23:33:19 -0400

    Use new Promise method instead of fs.promises.readFile

--- 0x12-Node_JS_basic/7-http_express.js
+++ 0x12-Node_JS_basic/7-http_express.js
@@ -1,54 +1,55 @@
 const express = require('express');
 const fs = require('fs');
 
-async function countStudents(filepath) {
-  try {
-    const csv = await fs.promises.readFile(filepath, { encoding: 'utf8' });
-    const headerArray = csv.split(/\r?\n|\n/);
-    const headers = headerArray[0].split(',');
+function countStudents(filepath) {
+  return new Promise((resolve, reject) => {
+    fs.readFile(filepath, 'utf-8', (err, res) => {
+      if (err) return reject(new Error('Cannot load the database'));
 
-    // strip headers and convert to list of dicts
-    const dictList = [];
-    const noHeaderArray = headerArray.slice(1);
-    for (let i = 0; i < noHeaderArray.length; i += 1) {
-      const data = noHeaderArray[i].split(',');
-      if (data.length === headers.length) {
-        const row = {};
-        for (let j = 0; j < headers.length; j += 1) {
-          row[headers[j].trim()] = data[j].trim();
+      const headerArray = res.split(/\r?\n|\n/);
+      const headers = headerArray[0].split(',');
+
+      // strip headers and convert to list of dicts
+      const dictList = [];
+      const noHeaderArray = headerArray.slice(1);
+      for (let i = 0; i < noHeaderArray.length; i += 1) {
+        const data = noHeaderArray[i].split(',');
+        if (data.length === headers.length) {
+          const row = {};
+          for (let j = 0; j < headers.length; j += 1) {
+            row[headers[j].trim()] = data[j].trim();
+          }
+          dictList.push(row);
         }
-        dictList.push(row);
       }
-    }
 
-    // count and collect first names of students per field
-    let countCS = 0;
-    let countSWE = 0;
-    const studentsCS = [];
-    const studentsSWE = [];
+      // count and collect first names of students per field
+      let countCS = 0;
+      let countSWE = 0;
+      const studentsCS = [];
+      const studentsSWE = [];
 
-    dictList.forEach((element) => {
-      if (element.field === 'CS') {
-        countCS += 1;
-        studentsCS.push(element.firstname);
-      } else if (element.field === 'SWE') {
-        countSWE += 1;
-        studentsSWE.push(element.firstname);
-      }
+      dictList.forEach((element) => {
+        if (element.field === 'CS') {
+          countCS += 1;
+          studentsCS.push(element.firstname);
+        } else if (element.field === 'SWE') {
+          countSWE += 1;
+          studentsSWE.push(element.firstname);
+        }
+      });
+
+      const countStudents = countCS + countSWE;
+
+      return resolve({
+        countStudents,
+        countCS,
+        countSWE,
+        studentsCS,
+        studentsSWE,
+      });
     });
-
-    const countStudents = countCS + countSWE;
-
-    return ({
-      countStudents,
-      countCS,
-      countSWE,
-      studentsCS,
-      studentsSWE,
-    });
-  } catch (err) {
-    throw new Error('Cannot load the database');
-  }
+  });
 }
 
 const pathToDB = process.argv[2];
@@ -59,9 +60,9 @@ app.get('/', (req, res) => {
   res.send('Hello Holberton School!');
 });
 
-app.get('/students', (req, res) => {
+app.get('/students', async (req, res) => {
   // call async function and collect needed variables
-  countStudents(pathToDB)
+  await countStudents(pathToDB)
     .then(({
       countStudents,
       countCS,
@@ -73,7 +74,7 @@ app.get('/students', (req, res) => {
       const total = `Number of students: ${countStudents}\n`;
       const CS = `Number of students in CS: ${countCS}. List: ${studentsCS.toString().split(',').join(', ')}\n`;
       const SWE = `Number of students in SWE: ${countSWE}. List: ${studentsSWE.toString().split(',').join(', ')}`;
-      res.send(text + total + CS + SWE);
+      res.status(200).send(text + total + CS + SWE);
     })
     .catch(() => {
       res.status(404).send('Cannot load the database');
