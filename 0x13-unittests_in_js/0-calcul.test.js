bc2884047184914b80382f6d02754bbc8422a94f
Gareth Brickman <brickstick@gmail.com>
2020-10-04 18:18:03 -0400

    Handle NaN

--- 0x13-unittests_in_js/0-calcul.test.js
+++ 0x13-unittests_in_js/0-calcul.test.js
@@ -5,5 +5,12 @@ const assert = require("assert");
 describe('calculateNumber', () => {
     it('returns rounded sum', () => {
     assert.strictEqual(calculateNumber(1, 3), 4);
+    assert.strictEqual(calculateNumber(1.6, 3), 5);
+    assert.strictEqual(calculateNumber(1.2, 3.8), 5);
+    assert.strictEqual(calculateNumber(-1, -3), -4);
+    assert.strictEqual(calculateNumber(-1.4, -3.6), -5);
+    });
+    it('should throw error if NaN passed', function () {
+        assert.throws(() => calculateNumber(NaN, 3), '[Function: TypeError]');
     });
 });
